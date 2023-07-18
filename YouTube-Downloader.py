023364011282c7da807b239d6f6402214b4fee98
Jose Ramirez <rosejamirez27@gmail.com>
2023-07-18 15:14:18 -0400

    Added a try/except block for the downloader

--- YouTube-Downloader.py
+++ YouTube-Downloader.py
@@ -4,16 +4,24 @@ from pytube import YouTube
 # Get the YouTube link from the user
 youtube_link = input("Please enter the YouTube link: ")
 
-# Create a YouTube object for the video corresponding to the given link
-youtube_video = YouTube(youtube_link)
+try:
+    # Create a YouTube object for the video corresponding to the given link
+    youtube_video = YouTube(youtube_link)
 
-# Print the video's title and view count
-print('Title: ', youtube_video.title)
-print('Views: ', youtube_video.views)
+    # Print the video's title
+    print('Title: ', youtube_video.title)
 
-# Get the video stream with the highest resolution
-highest_res_stream = youtube_video.streams.get_highest_resolution()
+    # Get the video stream with the highest resolution
+    highest_res_stream = youtube_video.streams.get_highest_resolution()
 
-# Download the video and save it to the specified path
-download_path = "/Users/Galock/Desktop/CS_Summer/Downloaded-Videos"
-highest_res_stream.download(download_path)
+    # Download the video and save it to the specified path
+    download_path = "/Users/Galock/Desktop/CS_Summer/Downloaded-Videos"
+    highest_res_stream.download(download_path)
+
+    # Print a success message
+    print("Your video has downloaded!")
+
+except Exception as e:
+    # If an error occurred, print an error message
+    print("The video failed to download... :(")
+    print("Error details: ", str(e))
